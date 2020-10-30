bcc5ad0268d621049b5cb84d3e00b41781539abd
yikezawa <y.ikezawa93@gmail.com>
2020-10-30 23:23:00 +0900

    mod

--- lesson13/one/main.go
+++ lesson13/one/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"reflect"
 	"unsafe"
 )
@@ -38,8 +39,70 @@ func equal(x, y reflect.Value, seen map[comparison]bool) bool {
 		seen[c] = true
 	}
 
-	// todo
+	switch x.Kind() {
+	case reflect.Bool:
+		return x.Bool() == y.Bool()
+
+	case reflect.String:
+		return x.String() == y.String()
+
+	case reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64:
+		return x.Int() == y.Int()
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return x.Uint() == y.Uint()
+
+	case reflect.Float32, reflect.Float64:
+		return equalFloat(x.Float(), y.Float())
+
+	case reflect.Complex64, reflect.Complex128:
+		return equalComplex(x.Complex(), y.Complex())
+
+	case reflect.Chan, reflect.UnsafePointer, reflect.Func:
+		return x.Pointer() == y.Pointer()
+
+	case reflect.Ptr, reflect.Interface:
+		return equal(x.Elem(), y.Elem(), seen)
+
+	case reflect.Array, reflect.Slice:
+		if x.Len() != y.Len() {
+			return false
+		}
+		for i := 0; i < x.Len(); i++ {
+			if !equal(x.Index(i), y.Index(i), seen) {
+				return false
+			}
+		}
+		return true
+
+	case reflect.Map:
+		if x.Len() != y.Len() {
+			return false
+		}
+		for _, k := range x.MapKeys() {
+			if !equal(x.MapIndex(k), y.MapIndex(k), seen) {
+				return false
+			}
+		}
+		return true
+	}
+
 	panic("unreachable")
+}
+
+func equalFloat(x, y float64) bool {
+	return math.Abs(x-y) <= eps
+}
+
+func equalComplex(x, y complex128) bool {
+	return equalFloat(real(x), real(y)) && equalFloat(imag(x), imag(y))
+}
+
+func Equal(x, y interface{}) bool {
+	seen := make(map[comparison]bool)
+	return equal(reflect.ValueOf(x), reflect.ValueOf(y), seen)
 }
 
 type comparison struct {

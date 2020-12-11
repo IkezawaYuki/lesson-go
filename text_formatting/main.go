3d1d2289f332eadce89a5993d27b6fba8015efa4
yikezawa <y.ikezawa93@gmail.com>
2020-12-11 22:49:08 +0900

    mod

---
+++ text_formatting/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"strings"
+	"unicode"
+)
+
+type FormattedText struct {
+	plainText  string
+	capitalize []bool
+}
+
+func (f *FormattedText) String() string {
+	sb := strings.Builder{}
+	for i := 0; i < len(f.plainText); i++ {
+		c := f.plainText[i]
+		if f.capitalize[i] {
+			sb.WriteRune(unicode.ToUpper(rune(c)))
+		} else {
+			sb.WriteRune(rune(c))
+		}
+	}
+	return sb.String()
+}
+
+func (f *FormattedText) Capitalize(start, end int) {
+	for i := start; i <= end; i++ {
+		f.capitalize[i] = true
+	}
+}
+
+type TextRange struct {
+	Start, End               int
+	Capitalize, Bold, Italic bool
+}
+
+func (t *TextRange) Covers(position int) bool {
+
+}
+
+func main() {
+
+}

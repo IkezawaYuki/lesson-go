02613d3852a4ea3cefc858e35a0c2fdfdce0185f
yikezawa <y.ikezawa93@gmail.com>
2020-12-20 23:18:36 +0900

    mod

--- command/main.go
+++ command/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var overdraftLimit = -500
 
@@ -22,6 +24,13 @@ func (b *BankAccount) WithDraw(amount int) bool {
 	return false
 }
 
+type Command interface {
+	Call()
+	Undo()
+	Succeeded() bool
+	SetSucceeded(value bool)
+}
+
 type Action int
 
 const (
@@ -34,6 +43,14 @@ type BankAccountCommand struct {
 	action    Action
 	amount    int
 	succeeded bool
+}
+
+func (b *BankAccountCommand) Succeeded() bool {
+	return b.succeeded
+}
+
+func (b *BankAccountCommand) SetSucceeded(value bool) {
+	b.succeeded = value
 }
 
 func (b *BankAccountCommand) Call() {
@@ -66,13 +83,85 @@ func NewBankAccountCommand(account *BankAccount, action Action, amount int) *Ban
 	}
 }
 
+type CompositeBankAccountCommand struct {
+	commands []Command
+}
+
+func (c *CompositeBankAccountCommand) Succeeded() bool {
+	for _, cmd := range c.commands {
+		if !cmd.Succeeded() {
+			return false
+		}
+	}
+	return true
+}
+
+func (c *CompositeBankAccountCommand) SetSucceeded(value bool) {
+	for _, cmd := range c.commands {
+		cmd.SetSucceeded(value)
+	}
+}
+
+func (c *CompositeBankAccountCommand) Call() {
+	for _, cmd := range c.commands {
+		cmd.Call()
+	}
+}
+
+func (c *CompositeBankAccountCommand) Undo() {
+	for idx := range c.commands {
+		c.commands[len(c.commands)-idx-1].Undo()
+	}
+}
+
+type MoneyTransferCommand struct {
+	CompositeBankAccountCommand
+	from, to *BankAccount
+	amount   int
+}
+
+func NewMoneyTransferCommand(from *BankAccount, to *BankAccount, amount int) *MoneyTransferCommand {
+	c := &MoneyTransferCommand{
+		from:   from,
+		to:     to,
+		amount: amount,
+	}
+	c.commands = append(c.commands, NewBankAccountCommand(from, Withdraw, amount))
+	c.commands = append(c.commands, NewBankAccountCommand(to, Deposit, amount))
+	return c
+}
+
+func (m *MoneyTransferCommand) Call() {
+	ok := true
+	for _, cmd := range m.commands {
+		if ok {
+			cmd.Call()
+			ok = cmd.Succeeded()
+		} else {
+			cmd.SetSucceeded(false)
+		}
+	}
+}
+
 func main() {
-	ba := BankAccount{}
-	cmd := NewBankAccountCommand(&ba, Deposit, 100)
-	cmd.Call()
+	ba := &BankAccount{}
+	cmdDeposit := NewBankAccountCommand(ba, Deposit, 100)
+	cmdWithDraw := NewBankAccountCommand(ba, Withdraw, 1000)
+	cmdDeposit.Call()
+	cmdWithDraw.Call()
+	fmt.Println(ba)
+	cmdWithDraw.Undo()
+	cmdDeposit.Undo()
 	fmt.Println(ba)
 
-	cmd2 := NewBankAccountCommand(&ba, Withdraw, 50)
-	cmd2.Call()
-	fmt.Println(ba)
+	from := BankAccount{100}
+	to := BankAccount{0}
+	mtc := NewMoneyTransferCommand(&from, &to, 100)
+	mtc.Call()
+
+	fmt.Println("from=", from, "to=", to)
+
+	fmt.Println("Undoing...")
+	mtc.Undo()
+	fmt.Println("from=", from, "to=", to)
 }

a2c6e080d334462075bdbcdba1aaa4e2e4e93081
yikezawa <y.ikezawa93@gmail.com>
2020-12-23 22:37:12 +0900

    mod

--- lexing/main.go
+++ lexing/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+)
 
 type TokenType int
 
@@ -61,11 +66,84 @@ func (t *Token) String() string {
 }
 
 func Lex(input string) []Token {
-	// todo
+	var result []Token
+
+	for i := 0; i < len(input); i++ {
+		switch input[i] {
+		case '+':
+			result = append(result, Token{Plus, "+"})
+		case '-':
+			result = append(result, Token{Minus, "-"})
+		case '(':
+			result = append(result, Token{Rparen, "("})
+		case ')':
+			result = append(result, Token{Lparen, ")"})
+		default:
+			sb := strings.Builder{}
+			for j := i; j < len(input); j++ {
+				if unicode.IsDigit(rune(input[j])) {
+					sb.WriteRune(rune(input[j]))
+					i++
+				} else {
+					result = append(result, Token{
+						Int, sb.String()})
+					i--
+					break
+				}
+			}
+		}
+	}
+	return result
+}
+
+func Parse(tokens []Token) Element {
+	result := BinaryOperation{}
+	haveLhs := false
+	for i := 0; i < len(tokens); i++ {
+		token := &tokens[i]
+		switch token.Type {
+		case Int:
+			n, _ := strconv.Atoi(token.Text)
+			integer := Integer{n}
+			if !haveLhs {
+				result.Left = &integer
+				haveLhs = true
+			} else {
+				result.Right = &integer
+			}
+		case Plus:
+			result.Type = Addition
+		case Minus:
+			result.Type = Subtraction
+		case Lparen:
+			j := i
+			for ; j < len(tokens); j++ {
+				if tokens[j].Type == Rparen {
+					break
+				}
+			}
+			var subexp []Token
+			for k := i + 1; k < j; k++ {
+				subexp = append(subexp, tokens[k])
+			}
+			element := Parse(subexp)
+			if !haveLhs {
+				result.Left = element
+				haveLhs = true
+			} else {
+				result.Right = element
+			}
+			i = j
+		}
+	}
+	return &result
 }
 
 func main() {
 	input := "(13+4)-(12-1)"
 	tokens := Lex(input)
 	fmt.Println(tokens)
+
+	parsed := Parse(tokens)
+	fmt.Printf("%s = %d\n", input, parsed.Value())
 }

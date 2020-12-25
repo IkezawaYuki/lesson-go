bc8b504c161af43329f26cb663a6539a87262f3f
yikezawa <y.ikezawa93@gmail.com>
2020-12-25 23:26:39 +0900

    mod

--- iterator/main.go
+++ iterator/main.go
@@ -10,6 +10,61 @@ func (p *Person) Names() []string {
 	return []string{p.FirstName, p.MiddleName, p.LastName}
 }
 
+type Node struct {
+	Value               int
+	left, right, parent *Node
+}
+
+func NewNode(value int, left *Node, right *Node) *Node {
+	n := &Node{
+		Value: value,
+		left:  left,
+		right: right,
+	}
+	left.parent = n
+	right.parent = n
+	return n
+}
+
+func NewTerminalNode(value int) *Node {
+	return &Node{Value: value}
+}
+
+type InOrderIterator struct {
+	Current     *Node
+	root        *Node
+	returnStart bool
+}
+
+func NewInOrderIterator(root *Node) *InOrderIterator {
+	i := &InOrderIterator{
+		Current:     root,
+		root:        root,
+		returnStart: false,
+	}
+	for i.Current.left != nil {
+		i.Current = i.Current.left
+	}
+	return i
+}
+
+func (i *InOrderIterator) Reset() {
+	i.Current = i.root
+	i.returnStart = false
+}
+
+func (i *InOrderIterator) MoveNext() bool {
+	if i.Current == nil {
+		return false
+	}
+	if !i.returnStart {
+		i.returnStart = true
+		return true
+	}
+
+	// todo
+}
+
 func main() {
 	p := Person{
 		"A", "B", "C",

76f9613a866ca8861a34f63b50ef552d6caede49
yikezawa <y.ikezawa93@gmail.com>
2020-12-27 17:26:48 +0900

    mod

--- memento/main.go
+++ memento/main.go
@@ -8,30 +8,68 @@ type Memento struct {
 
 type BankAccount struct {
 	balance int
+	changes []*Memento
+	current int
 }
 
-func NewBankAccount(balance int) (*BankAccount, *Memento) {
-	return &BankAccount{balance: balance}, &Memento{Balance: balance}
+func (b *BankAccount) String() string {
+	return fmt.Sprint("Balance = $", b.balance,
+		", current = ", b.current)
+}
+
+func NewBankAccount(balance int) *BankAccount {
+	b := &BankAccount{balance: balance}
+	b.changes = append(b.changes, &Memento{Balance: balance})
+	return b
 }
 
 func (b *BankAccount) Deposit(amount int) *Memento {
 	b.balance += amount
-	return &Memento{b.balance}
+	m := Memento{b.balance}
+	b.changes = append(b.changes, &m)
+	b.current++
+	fmt.Println("Deposited", amount, ", balance is now", b.balance)
+	return &m
 }
 
 func (b *BankAccount) Restore(m *Memento) {
-	b.balance = m.Balance
+	if m != nil {
+		b.balance -= m.Balance
+		b.changes = append(b.changes, m)
+		b.current = len(b.changes) - 1
+	}
+}
+
+func (b *BankAccount) Undo() *Memento {
+	if b.current > 0 {
+		b.current--
+		m := b.changes[b.current]
+		b.balance = m.Balance
+		return m
+	}
+	return nil
+}
+
+func (b *BankAccount) Redo() *Memento {
+	if b.current+1 < len(b.changes) {
+		b.current++
+		m := b.changes[b.current]
+		b.balance = m.Balance
+		return m
+	}
+	return nil
 }
 
 func main() {
-	ba := BankAccount{100}
-	m1 := ba.Deposit(50)
-	m2 := ba.Deposit(25)
+	ba := NewBankAccount(100)
+	ba.Deposit(50)
+	ba.Deposit(25)
 	fmt.Println(ba)
 
-	ba.Restore(m1)
+	ba.Undo()
 	fmt.Println(ba)
-
-	ba.Restore(m2)
+	ba.Undo()
+	fmt.Println(ba)
+	ba.Redo()
 	fmt.Println(ba)
 }
---
+++ observers/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+type Observable struct {
+	subs *list.List
+}
+
+func (o *Observable) Subscribe(x Observer) {
+	o.subs.PushBack(x)
+}
+
+func (o *Observable) Unsubscribe(x Observer) {
+	for z := o.subs.Front(); z != nil; z = z.Next() {
+		if z.Value.(Observer) == x {
+			o.subs.Remove(z)
+		}
+	}
+}
+
+func (o *Observable) Fire(data interface{}) {
+	for z := o.subs.Front(); z != nil; z = z.Next() {
+		z.Value.(Observer).Notify(data)
+	}
+}
+
+type Observer interface {
+	Notify(data interface{})
+}
+
+type Person struct {
+	Observable
+	age int
+}
+
+func NewPerson(age int) *Person {
+	return &Person{
+		Observable: Observable{new(list.List)},
+		age:        age,
+	}
+}
+
+type PropertyChanged struct {
+	Name  string
+	Value interface{}
+}
+
+func (p *Person) Age() int {
+	return p.age
+}
+
+func (p *Person) SetAge(age int) {
+	if age == p.age {
+		return
+	}
+	p.age = age
+	p.Fire(PropertyChanged{"Age", p.age})
+}
+
+type TrafficManagement struct {
+	o Observable
+}
+
+func (t *TrafficManagement) Notify(data interface{}) {
+	if pc, ok := data.(PropertyChanged); ok {
+		if pc.Value.(int) >= 18 {
+			fmt.Println("Congrats, you can drive now!")
+			t.o.Unsubscribe(t)
+		}
+	}
+}
+
+func main() {
+	p := NewPerson(15)
+	t := &TrafficManagement{p.Observable}
+	p.Subscribe(t)
+
+	for i := 16; i <= 20; i++ {
+		fmt.Println("Setting age to", i)
+		p.SetAge(i)
+	}
+}

b84e49c7949eb2627d883a44cd20e6b81917a4b2
yikezawa <y.ikezawa93@gmail.com>
2021-01-03 19:17:39 +0900

    mod

--- classic_visitor/main.go
+++ classic_visitor/main.go
@@ -1,9 +1,69 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 type ExpressionVisitor interface {
-	VisitorDoubleExpression(de *DoubleExpression)
+	VisitDoubleExpression(de *DoubleExpression)
+	VisitAdditionExpression(ae *AdditionExpression)
+}
+
+type Expression interface {
+	Accept(ev ExpressionVisitor)
+}
+
+type DoubleExpression struct {
+	value float64
+}
+
+func (d *DoubleExpression) Accept(ev ExpressionVisitor) {
+	ev.VisitDoubleExpression(d)
+}
+
+type AdditionExpression struct {
+	left, right Expression
+}
+
+func (a *AdditionExpression) Accept(ev ExpressionVisitor) {
+	ev.VisitAdditionExpression(a)
+}
+
+type ExpressionPrinter struct {
+	sb strings.Builder
+}
+
+func (e *ExpressionPrinter) VisitDoubleExpression(de *DoubleExpression) {
+	e.sb.WriteString(fmt.Sprintf("%g", de.value))
+}
+
+func (e *ExpressionPrinter) VisitAdditionExpression(ae *AdditionExpression) {
+	e.sb.WriteString("(")
+	ae.left.Accept(e)
+	e.sb.WriteString("+")
+	ae.right.Accept(e)
+	e.sb.WriteString(")")
+}
+
+func NewExpressionPrinter() *ExpressionPrinter {
+	return &ExpressionPrinter{strings.Builder{}}
+}
+
+func (e *ExpressionPrinter) String() string {
+	return e.sb.String()
 }
 
 func main() {
-
+	e := &AdditionExpression{
+		left: &DoubleExpression{1},
+		right: &AdditionExpression{
+			left:  &DoubleExpression{2},
+			right: &DoubleExpression{3},
+		},
+	}
+	ep := NewExpressionPrinter()
+	ep.VisitAdditionExpression(e)
+	fmt.Println(ep.String())
+	fmt.Printf("%s\n", ep)
 }

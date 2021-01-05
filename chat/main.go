8927e61fc1f5361b21178b53afb3ea863dc9deb7
yikezawa <y.ikezawa93@gmail.com>
2021-01-05 22:24:06 +0900

    mod

--- chat/main.go
+++ chat/main.go
@@ -1,9 +1,34 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":8080", "http serve address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "not found", http.StatusNotFound)
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
+	http.ServeFile(w, r, "html")
+}
 
 func main() {
 	flag.Parse()
 	hub := newHub()
 	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatalln("Listen and serve:", err)
+	}
 }

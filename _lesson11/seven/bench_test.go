775845e334ad52b282bd019c8f0847e70d96cb58
yikezawa <y.ikezawa93@gmail.com>
2021-01-13 20:46:52 +0900

    mod

--- _lesson11/seven/bench_test.go
+++ _lesson11/seven/bench_test.go
@@ -1,5 +1,33 @@
-package main
+package intset
 
-func unionWith_MapIntSet() {
+import (
+	"math"
+	"math/rand"
+	"testing"
+	"time"
+)
 
+var seed int64 = time.Now().UTC().UnixNano()
+
+func BenchmarkIntSet_Add(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		add_IntSet()
+	}
+}
+
+func add_IntSet() {
+	var set IntSet
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 500; i++ {
+		set.Add(rng.Intn(math.MaxInt16))
+	}
+}
+
+func add_MapIntSet() {
+	var set MapIntSet
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < 500; i++ {
+		set.Add(rng.Intn(math.MaxInt16))
+	}
 }

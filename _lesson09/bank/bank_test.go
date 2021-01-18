72b84197c4441397c3eeba9da54dd10b363e43e2
yikezawa <y.ikezawa93@gmail.com>
2021-01-18 23:09:26 +0900

    mod

--- _lesson09/bank/bank_test.go
+++ _lesson09/bank/bank_test.go
@@ -1,7 +1,52 @@
 package bank
 
-import "testing"
+import (
+	"fmt"
+	"github.com/YoshikiShibata/gpl/ch09/ex01/bank"
+	"testing"
+)
 
 func TestWithdrawNormal(t *testing.T) {
+	bank.Deposit(200)
+	ok := bank.Withdraw(100)
+	if !ok {
+		t.Error("Result is false, want true")
+		return
+	}
+	ok = bank.Withdraw(100)
+	if !ok {
+		t.Error("Result is false, want true")
+		return
+	}
+	ok = bank.Withdraw(100)
+	if ok {
+		t.Error("Result is true, want false")
+		return
+	}
+	if bank.Balance() != 0 {
+		t.Errorf("Result is %d, want 0", bank.Balance())
+	}
+	fmt.Println(bank.Balance())
+}
 
+func TestBank(t *testing.T) {
+	done := make(chan struct{})
+
+	go func() {
+		bank.Deposit(200)
+		fmt.Println("=", bank.Balance())
+		done <- struct{}{}
+	}()
+
+	go func() {
+		bank.Deposit(100)
+		done <- struct{}{}
+	}()
+
+	<-done
+	<-done
+
+	if got, want := bank.Balance(), 300; got != want {
+		t.Errorf("Balance = %d, want %d", got, want)
+	}
 }

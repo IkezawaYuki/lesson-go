b3d24fb53ed0acbdd0dc21b24454520d8965d6b3
yikezawa <y.ikezawa93@gmail.com>
2021-02-02 23:27:22 +0900

    mod

--- errors_mirror/wrap_mirror.go
+++ errors_mirror/wrap_mirror.go
@@ -1,1 +1,26 @@
 package errors_mirror
+
+import "unsafe"
+
+func Unwrap(err error) error {
+	u, ok := err.(interface {
+		Unwrap() error
+	})
+	if !ok {
+		return nil
+	}
+	return u.Unwrap()
+}
+
+func Is(err, target error) bool {
+	if target == nil {
+		return err == target
+	}
+	// todo
+	return false
+}
+
+type emptyinterface struct {
+	typ  *rtype
+	word unsafe.Pointer
+}

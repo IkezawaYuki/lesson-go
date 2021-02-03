8addc6a669a4df5f31c9b7754c36b3bd8d45a819
yikezawa <y.ikezawa93@gmail.com>
2021-02-03 23:00:03 +0900

    mod

--- errors_mirror/wrap_mirror.go
+++ errors_mirror/wrap_mirror.go
@@ -1,6 +1,6 @@
 package errors_mirror
 
-import "unsafe"
+import "reflect"
 
 func Unwrap(err error) error {
 	u, ok := err.(interface {
@@ -16,11 +16,42 @@ func Is(err, target error) bool {
 	if target == nil {
 		return err == target
 	}
-	// todo
+
+	isComparable := reflect.TypeOf(target).Comparable()
+	for {
+		if isComparable && err == target {
+			return true
+		}
+
+	}
 	return false
 }
 
-type emptyinterface struct {
-	typ  *rtype
-	word unsafe.Pointer
+func As(err error, target interface{}) bool {
+	if target == nil {
+		panic("errors: target cannot be nil")
+	}
+	val := reflect.ValueOf(target)
+	typ := val.Type()
+	if typ.Kind() != reflect.Ptr || val.IsNil() {
+		panic("errors: target must be a non-nil pointer")
+	}
+
+	if e := typ.Elem(); e.Kind() != reflect.Interface && !e.Implements(errorType) {
+		panic("error: *target must be interface or implement error")
+	}
+	targetType := typ.Elem()
+	for err != nil {
+		if reflect.TypeOf(err).AssignableTo(targetType) {
+			val.Elem().Set(reflect.ValueOf(err))
+			return true
+		}
+		if x, ok := err.(interface{ As(interface{}) bool }); ok && x.As(target) {
+			return true
+		}
+		err = Unwrap(err)
+	}
+	return false
 }
+
+var errorType = reflect.TypeOf((*error)(nil)).Elem()

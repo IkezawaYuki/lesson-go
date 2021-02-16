ad131877018d4f749a2f0158d523e34d95abb5c3
yikezawa <y.ikezawa93@gmail.com>
2021-02-16 23:12:40 +0900

    mod

--- path_mirror/path_mirror.go
+++ path_mirror/path_mirror.go
@@ -25,3 +25,62 @@ func (b *lazybuf) append(c byte) {
 	b.buf[b.w] = c
 	b.w++
 }
+
+func (b *lazybuf) string() string {
+	if b.buf == nil {
+		return b.s[:b.w]
+	}
+	return string(b.buf[:b.w])
+}
+
+func Clean(path string) string {
+	if path == "" {
+		return "."
+	}
+
+	rooted := path[0] == '/'
+	n := len(path)
+
+	out := lazybuf{s: path}
+	r, dotdot := 0, 0
+	if rooted {
+		out.append('/')
+	}
+
+	for r < n {
+		switch {
+		case path[r] == '/':
+			r++
+		case path[r] == '.' && (r+1 == n || path[r+1] == '/'):
+			r++
+		case path[r] == '.' && path[r+1] == '.' && (r+2 == n || path[r+2] == '/'):
+			r += 2
+			switch {
+			case out.w > dotdot:
+				out.w--
+				for out.w > dotdot && out.index(out.w) != '/' {
+					out.w--
+				}
+			case !rooted:
+				if out.w > 0 {
+					out.append('/')
+				}
+				out.append('.')
+				out.append('.')
+				dotdot = out.w
+			}
+		default:
+			if rooted && out.w != 1 || !rooted && out.w != 0 {
+				out.append('/')
+			}
+			for ; r < n && path[r] != '/'; r++ {
+				out.append(path[r])
+			}
+		}
+	}
+
+	if out.w == 0 {
+		return "."
+	}
+	return out.string()
+}
